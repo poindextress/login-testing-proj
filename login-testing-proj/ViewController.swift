48ab7719967add3a322b61d7513a301dc8831356
Ashlei <shuryjo@gmail.com>
2022-03-26 16:44:37 -0400

    Created a basic login interface using Storyboard/Outlets/etc. via the Interface Builder

--- login-testing-proj/ViewController.swift
+++ login-testing-proj/ViewController.swift
@@ -9,11 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
 }
-
