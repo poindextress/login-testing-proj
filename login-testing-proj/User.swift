5494d5f5bd480d2dd9b60f70bb5a413400488013
Ashlei <shuryjo@gmail.com>
2022-03-26 16:50:53 -0400

    created a User struct

---
+++ login-testing-proj/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  login-testing-proj
+//
+//  Created by Ashlei Mitchell on 3/26/22.
+//
+
+import Foundation
+
+struct User {
+    let email: String
+    let password: String
+}
