8e9670c202a4f90966b04604e3b0e5c1928a2d43
Ashlei <shuryjo@gmail.com>
2022-03-28 15:05:11 -0400

    Added a do-catch statement that will produce a success/failure alert/error depending on if the user correctly inputs their credentials. The only success criteria is that it matches the User email/password via the "tempDatabase" (line 15)

--- login-testing-proj/ViewController.swift
+++ login-testing-proj/ViewController.swift
@@ -15,7 +15,49 @@ class ViewController: UIViewController {
     private let tempDatabase = [User(email: "shury@thatsme.com", password: "avocadoGood")]
     
     @IBAction func didTapLoginButton() {
-        print("You tapped the log-in button :D")
+        do {
+            guard
+                let email = emailTextField.text,
+                let password = passwordTextField.text
+                else { throw ValidationError.invalidCredentials }
+            
+            if let user = tempDatabase.first(where: { user in
+                user.password == password && user.email == email
+            }) {
+                presentAlert(with: "You successfully logged in as \(user.email)")
+                
+            } else {
+                throw ValidationError.invalidCredentials
+            }
+        } catch {
+            presentAlert(with: error.localizedDescription)
+        }
     }
+}
 
+extension ViewController {
+    enum ValidationError: LocalizedError {
+        case invalidCredentials
+        
+        var errorDescription: String? {
+            switch self {
+            case .invalidCredentials:
+                return "You have entered invalid credentials."
+            }
+        }
+    }
+}
+
+extension UIViewController {
+    
+    private func present(_ dismissableAlert: UIAlertController) {
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel)
+        dismissableAlert.addAction(dismissAction)
+        present(dismissableAlert, animated: true)
+    }
+    
+    func presentAlert(with message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        present(alert)
+    }
 }
