a73e4d60049cbd15f10d65c4b6ebd87b1cfd63c4
Ashlei <shuryjo@gmail.com>
2022-03-28 16:17:36 -0400

    Added a function to check for valid e-mail address format when logging in. Added cases/guard statements that check for valid email address format as well as password length requirements. They will produce appropriate alerts if error is thrown.

--- login-testing-proj/ViewController.swift
+++ login-testing-proj/ViewController.swift
@@ -15,17 +15,21 @@ class ViewController: UIViewController {
     private let tempDatabase = [User(email: "shury@thatsme.com", password: "avocadoGood")]
     
     @IBAction func didTapLoginButton() {
+        
         do {
             guard
                 let email = emailTextField.text,
                 let password = passwordTextField.text
                 else { throw ValidationError.invalidCredentials }
-            
+
+            guard checkIfValidEmail(input: email) else { throw ValidationError.emailFormatBad }
+            guard password.count > 6 else { throw ValidationError.passwordTooShort }
+            guard password.count < 20 else { throw ValidationError.passwordTooLong }
+
             if let user = tempDatabase.first(where: { user in
                 user.password == password && user.email == email
             }) {
                 presentAlert(with: "You successfully logged in as \(user.email)")
-                
             } else {
                 throw ValidationError.invalidCredentials
             }
@@ -38,13 +42,32 @@ class ViewController: UIViewController {
 extension ViewController {
     enum ValidationError: LocalizedError {
         case invalidCredentials
+        case emailFormatBad
+        case passwordTooShort
+        case passwordTooLong
         
         var errorDescription: String? {
             switch self {
             case .invalidCredentials:
                 return "You have entered invalid credentials."
+            case .emailFormatBad:
+                return "You have entered an invalid e-mail address."
+            case .passwordTooShort:
+                return "Your password must be at least 6 characters."
+            case .passwordTooLong:
+                return "Your password cannot be more than 20 characters."
             }
         }
+    }
+    
+    func checkIfValidEmail(input: String) -> Bool {
+        let range = NSRange(location: 0, length: input.utf16.count)
+        let pattern = #"^[A-Za-z0-9]+([\.\-_][A-Za-z0-9]+)*@[A-Za-z0-9]+([\.\-_][A-Za-z0-9]+)*\.[A-Za-z][A-Za-z]+$"#
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        if regex.firstMatch(in: input, options: [], range: range) != nil {
+            return true
+        }
+        return false
     }
 }
 
